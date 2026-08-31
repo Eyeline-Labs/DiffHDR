b7bdc67ef9aefd20bc1568140887a0ab45344ce7
Yzmblog <1204994059@qq.com>
2026-08-31 16:35:29 -0500

    Add DiffHDR project website

--- diffsynth/pipelines/__init__.py
+++
@@ -1,1 +0,0 @@
-from .wan_video_new import WanVideoPipeline
--- utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .io import load_image, save_image
-from .color_utils import *
-
-__all__ = [
-    'load_image',
-    'save_image',
-]
